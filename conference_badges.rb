510d9c0ef3fd86d5d14ce08d4d12c81f69bdd431
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-20 03:34:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+rooms = 1
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}".
+end 
+
+def batch_badge_creator
+  name.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+  
+def assign_rooms
+  puts "Hello, #{name}, You'll be assigned to room #{rooms}"
+    rooms += 1 
+  end 
+
+def printer
+  name.each do |name|
+    puts "Hello, my name is #{name}."
+    puts "Hello, #{name}, You'll be assigned to room #{rooms}."
+    rooms +=1
